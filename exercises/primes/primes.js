d7406bf480a5d10c37283801fb73df7d180cc9ea
8Firdavs8 <atabfi01@luther.edu>
2020-12-06 14:25:05 -0600

    Final version

--- exercises/primes/primes.js
+++ exercises/primes/primes.js
@@ -2,6 +2,12 @@
 /* jshint node: true */
 'use strict';
 
+
+
+//Firdavs Atabaev
+//CS330: Exercise#1
+//Professor Yasinovskyy
+
 var urlParams = new URLSearchParams(window.location.search);
 
 function greet() {
@@ -9,8 +15,7 @@ function greet() {
     const urlParams = new URLSearchParams(queryString);
     let greetElement = document.querySelector('h1');
     if (urlParams.get("name") > ""){
-        greetElement.innerHTML = 'Hello' + " " + urlParams.get("name");
-       
+        greetElement.innerHTML = 'Hello' + " " + urlParams.get("name");   
     }  
     else{
         greetElement.innerHTML = 'Hello student';
@@ -33,13 +38,14 @@ function printPrimeNumber() {
     const queryNumber = window.location.search;
     const urlParams = new URLSearchParams(queryNumber);
     let printElement = document.querySelector("p");
-    let n = urlParams.get("n");
+    let n = urlParams.get("n") || 330;
     
-    if (isPrime(n) == true){
+    if (isPrime(n)){
         printElement.innerHTML = n + " is a prime number";
     }else {
         printElement.innerHTML = n + " is not a prime number";
-    } 
+    }
+   
 }
 
 function getNPrimes(n) {
@@ -59,16 +65,22 @@ function getNPrimes(n) {
 function printNPrimes() {
     const queryNumber = window.location.search;
     const urlParams = new URLSearchParams(queryNumber);
-    let primeElement = document.querySelector("table");
+    let primeElement = document.querySelector("table tbody");
     //primeElement.innerHTML = urlParams.get("n");
 
     let n = urlParams.get("n");
-    let y = [getNPrimes(n)];
-    
+    let y = getNPrimes(n);
+    /*
     for (let i = 0; i<y.length; i++){
         primeElement.innerHTML = y[i];
        
     }
+    */
+   for (let i = 0; i<y.length; i++){
+    primeElement.innerHTML += `<tr><td>${y[i]}</td></tr>`;
+   }
+   const tableHeader = document.querySelector("table thead");
+    tableHeader.innerHTML = `<b>${n} number of primes</b>`;
 }
 
 
