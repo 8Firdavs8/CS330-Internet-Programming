fde642c53c2f70374b876dd4b8bf96b69e28c4b3
8Firdavs8 <atabfi01@luther.edu>
2020-12-13 22:26:57 -0600

    project1

--- projects/calculator/js/calculator.js
+++ projects/calculator/js/calculator.js
@@ -3,22 +3,55 @@
 /* jshint browser: true */
 'use strict';
 
+
+/* 
+Firdavs Atabaev
+CS330: Project1
+Professor: Yasinovskyy
+*/
+
 var screen;
 
 
+  
+
 function enterDigit(digit) {
-}
+    //screen.innerHTML = digit;
+    let currentDigit = (document.querySelector("#result")).textContent;
+    //let numToDig = currentDigit.textContent();
+    let number = currentDigit.toString() + digit.toString();
+    screen.innerHTML = number; 
+   
+};
+
+
+
 
 function clear_screen() {
-}
+    screen.innerHTML = '';
+};
 
 function eval_expr() {
-}
+    let firstCalc = (document.getElementById("result")).textContent;
+    //let firstCalcToNum = firstCalcToNum.Number();
+    //console.log(firstCalcToNum);
+    let evaluate = eval(firstCalc.toString());
+    //let number = number(evaluate);
+    screen.innerHTML = evaluate;
+   
+
+
+};
 
 function enterOp(operation) {
-}
+    //screen.innerHTML = operation;
+    let currentOperand = (document.querySelector("#result")).textContent;
+    let operand = currentOperand.toString() + operation.toString();
+    screen.innerHTML = operand;
+};
 
 window.onload = function () {
     screen = document.querySelector("#result");
     screen.innerHTML = "0";
+   
 };
