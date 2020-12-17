60128167b259b12eec238ad3d8fa13e01975911e
8Firdavs8 <atabfi01@luther.edu>
2020-12-17 10:27:50 -0600

    exercise

--- exercises/promises/js/script.js
+++ exercises/promises/js/script.js
@@ -3,20 +3,81 @@
 /* jshint node: true */
 'use strict';
 
+
+async function getData(url) {
+    return fetch(url)
+        .then(response => response.json())
+        .catch(error => console.error(error));
+}
+
 async function get_individual(num, all_numbers) {
+
+    let allTheNumbers =  document.querySelector('#number_info');
+
+    for (let i = num - 1; i <= num + 1; i++) {
+
+        let info = await getData(`http://numbersapi.com/${i}?json`);
+
+        let infoPar = document.createElement("p");
+        infoPar.classList.add("row");
+
+        let numberColumn = document.createElement("p");
+        numberColumn.classList.add("col-3", "number-col");
+        numberColumn.innerText = i;
+        infoPar.appendChild(numberColumn);
+
+        let infoColumn = document.createElement("p");
+        infoColumn.classList.add("col-9", "info-col");
+        infoColumn.innerText = info["text"];
+        infoPar.appendChild(infoColumn);
+
+        allTheNumbers.appendChild(infoPar);
+
+    }
+    
+
 
 }
 
 async function get_batch(num, all_numbers) {
 
+    let allTheNumbers =  document.querySelector('#number_info');
+
+    let checkBox = await getData(`http://numbersapi.com/${num - 1}..${num + 1}?json`);
+    
+    for (let i = num - 1; i <= num + 1; i++) {
+        let infoPar = document.createElement("p");
+        infoPar.classList.add("row")
+
+        let numbersColumn = document.createElement("p");
+        numbersColumn .classList.add("col-3", "number-col");
+        numbersColumn .innerText = i;
+        infoPar.appendChild(numbersColumn);
+
+        let infoColumn = document.createElement("p");
+        infoColumn.classList.add("col-9", "info-col");
+        infoColumn.innerHTML = checkBox[i];
+        infoPar.appendChild(infoColumn);
+
+        
+        allTheNumbers.appendChild(infoPar);
+    }
+
+
 }
 
 async function clickedon() {
+    clear();
     let num = parseInt(document.querySelector('#number').value);
-    let all_numbers = document.querySelector('#number_info');
+    let allTheNumbers = document.querySelector('#number_info');
     if (document.querySelector('#batch').checked) {
-        get_batch(num, all_numbers);
+        await get_batch(num, allTheNumbers);
     } else {
-        get_individual(num, all_numbers);
+        await get_individual(num, allTheNumbers);
     }
+}
+
+function clear() {
+    let allTheNumbers = document.querySelector('#number_info');
+    allTheNumbers.innerHTML = "";
 }
