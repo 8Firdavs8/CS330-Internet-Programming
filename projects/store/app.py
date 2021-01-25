de33576684617f4c2ade66704462a326cec59e2d
8Firdavs8 <atabfi01@luther.edu>
2021-01-24 19:51:28 -0600

    final version

---
+++ projects/store/app.py
@@ -0,0 +1,76 @@
+import sqlite3
+from flask import render_template, url_for, make_response, Flask, request
+import csv
+import os
+import pandas as pd
+
+app = Flask(__name__)
+
+def read_txt(filename):
+    """File format: name category number"""
+    myList = []
+    with open(filename, "r") as data:
+        for line in data:
+            myList.append(tuple(line.strip().split(", ")))
+    return myList[1:]
+
+@app.route("/")
+def index():
+    if request.method == "GET":
+        return render_template("base.html")
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    name = request.args.get("name")
+    category = request.args.get("category")
+    number = request.args.get("number")
+
+    if name and category and number:
+        sum = str(name) + "," + str(category) +  "," + str(number)
+        appendFile = open('roster.csv', 'a')
+        appendFile.write(sum)
+        appendFile.write('\n')
+        appendFile.close()
+        with sqlite3.connect(f"roster.db") as conn:
+            conn.execute(f"drop table if exists roster")
+            data = pd.read_csv(f"roster.csv", header=None)
+            data.to_sql(f"roster", conn)   
+            cur = conn.cursor()
+            cur.execute(f"select * from roster")
+            emptyList = []
+            for name in cur:
+                name = name[1:]
+                emptyList.append(name)
+            return render_template("add.html", name=emptyList)  
+    else:
+        return render_template("add.html")
+
+@app.route("/list")
+def listOfThings():
+        with sqlite3.connect(f"roster.db") as conn:
+                cur = conn.cursor()
+                cur.execute(f"select * from roster")
+                if cur:
+                        emptyList = []
+                        for name in cur:
+                                name = name[1:]
+                                emptyList.append(name)
+                        if len(emptyList) == 0:
+                                text = "No DATA!"
+                                return render_template("list.html", removed=text)
+                        return render_template("list.html", a=emptyList)
+               
+
+@app.route("/remove")
+def removeFromList():
+        try:
+                with sqlite3.connect(f"roster.db") as conn:
+                        cur = conn.cursor()
+                        cur.execute(f"delete from roster")
+                       
+                        os.remove("roster.csv") 
+                        message = "Successfully removed all the items from DB, Please Check Costumer"
+                        return render_template("base.html", myMessage=message)
+        except FileNotFoundError:
+                cant = "You Can't Remove from an Empty table"
+                return render_template("base.html", myMessage=cant)
